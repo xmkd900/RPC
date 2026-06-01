6589cf6fbc694cb4785e0395c0e958ca94ae1810
ljm <3321481586@qq.com>
2026-06-01 21:21:51 +0800

    基础动态代理及反射的使用

---
+++ src/main/java/com/rpc/Client/proxy/ClientProxy.java
@@ -0,0 +1,44 @@
+package com.rpc.Client.proxy;
+
+import com.rpc.Client.IOClient;
+import com.rpc.Common.Message.RPCrequest;
+import com.rpc.Common.Message.RPCresponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ClientProxy implements InvocationHandler {
+    private String host;
+    private int port;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //封装rpcrequest请求
+        RPCrequest rpCrequest=RPCrequest.builder()
+                .interfaceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameters(args)
+                .parameterTypes(method.getParameterTypes())
+                .build();
+        RPCresponse response= IOClient.sendRequest(host,port,rpCrequest);
+        return response.getData();
+
+    }
+
+
+    public <T> T getProxy(Class<T>clazz){
+        return (T) Proxy.newProxyInstance(
+                clazz.getClassLoader(),
+                clazz.getInterfaces(),
+                this
+        );
+    }
+}
+
